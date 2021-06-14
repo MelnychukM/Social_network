531de746db8d6415b344770f68a6adff2de13007
MalnychukM <mishamail55558@gmail.com>
2021-06-14 20:37:37 +0300

    lesson 21 - Верстка Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog + " " + s.active}>
+                    Dmitry
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Misha
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your Mararader</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 
