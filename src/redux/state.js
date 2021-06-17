c6956478841dbef90f0025be54ef6d4c647c787a
MalnychukM <mishamail55558@gmail.com>
2021-06-17 21:06:11 +0300

    lesson 33 - Flux

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import {rerenderEntireTree} from "../render";
 
 let state = {
     profilePage: {
@@ -37,6 +38,7 @@ export let addPost = (postMessage) => {
         numberLike: 0
     };
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
